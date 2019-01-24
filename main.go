8cabe758ef121a76b205f4d01c21cf3332728b28
Alexey Simuskov <asimuskov@intermedia.net>
2019-01-24 11:43:16 +0300

    Added destination to file/gz-file. Refactored by using `filepath.Walk` - support declaration file as source. Added `-limit` key for control speed. But it's doesn't work as suppose =(

--- main.go
+++ main.go
@@ -3,93 +3,96 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
-	"path"
-	"sync"
+	"os/signal"
+	"path/filepath"
+	"runtime"
+	"syscall"
 	"time"
 )
 
 var (
-	workers      = flag.Int("workers", 1, "Amount of workers to process the files")
-	addr         = flag.String("addr", "localhost:8888", "UDP Address to send data")
+	addr         = flag.String("addr", "", "UDP Address to send data")
+	file         = flag.String("file", "", "Path to file. If path end with .gz - data will be compressed")
+	limit        = flag.Float64("limit", 0, "Messages per second to send. Multiply 1000. 0 is No limits")
 	pathToFiles  string
-	fileChan     chan string
-	workerPool   []*worker
-	group        sync.WaitGroup
 	workedFiles  int
 	totalFiles   int
 	droppedLines int
 	totalLines   int
+	done         = make(chan bool)
 )
+
+func init() {
+	runtime.GOMAXPROCS(1)
+}
 
 func main() {
 	start := time.Now()
 	flag.Parse()
-	fileChan = make(chan string, *workers)
 
 	pathToFiles = flag.Args()[0]
 	fmt.Printf("Args: %s\n", flag.Args())
-	// count Total
-	walkDir(pathToFiles, func(fl string) {
-		totalFiles++
-	})
-	fmt.Println("Total files: ", totalFiles)
-	startWorkers()
 
-	walkDir(pathToFiles, func(filename string) {
-		fileChan <- filename
-		workedFiles++
-		fmt.Printf("%v/%v of total\t\r", workedFiles, totalFiles)
-	})
-	fmt.Println("\nWait workers...")
-	close(fileChan)
-	group.Wait()
-	fmt.Printf("All files done! Time: %s,  Total lines: %v, Dropped: %v", time.Since(start), totalLines, droppedLines)
+	// count Total
+	filepath.Walk(pathToFiles, countFiles)
+	fmt.Println("Total files: ", totalFiles)
+
+	w := startWorkers()
+	go func() {
+		filepath.Walk(pathToFiles, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			w.WorkOnce(path)
+			workedFiles++
+			fmt.Printf("%v/%v of total\t\r", workedFiles, totalFiles)
+			return nil
+		})
+		done <- true
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt, os.Kill, syscall.SIGTERM)
+
+	select {
+	case <-quit:
+		log.Println("Wait worker done with current file")
+		w.Wait()
+		w.Close()
+	case <-done:
+		w.Close()
+		fmt.Println("All files done!")
+		break
+	}
+	fmt.Printf("Time: %s,  Total lines: %v, Dropped: %v\n", time.Since(start), totalLines, droppedLines)
+	fmt.Printf("Message per sec: %v\n", float64(totalLines)/time.Since(start).Seconds())
 	os.Exit(0)
 }
 
-func startWorkers() {
-	workerPool = make([]*worker, *workers)
-
-	for i := 0; i != *workers; i++ {
-
-		conn, err := getUDPConnection(*addr)
-		if err != nil {
-			closeWorkers(workerPool)
-			close(fileChan)
-			log.Fatal(err)
-		}
-		w := worker{id: i, con: conn, group: &group}
-		go w.Work(fileChan)
-		workerPool[i] = &w
+func startWorkers() *worker {
+	var writer io.WriteCloser
+	var err error
+	if *addr != "" {
+		writer, err = getUDPConnection(*addr)
+	} else if *file != "" {
+		writer, err = getGzFile(*file)
+	} else {
+		log.Fatal("No UDP address or FILE specified")
 	}
-}
-
-func closeWorkers(pool []*worker) {
-	for _, w := range pool {
-		if w != nil {
-			w.Close()
-		}
-	}
-}
-
-func walkDir(dir string, apply func(string)) {
-	var fullPath = func(part string) string {
-		return path.Join(pathToFiles, part)
-	}
-
-	files, err := ioutil.ReadDir(pathToFiles)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error with create writer: %s", err)
 	}
-	for _, f := range files {
-		if f.IsDir() {
-			continue
-			//walkDir(fullPath(f.Name()), apply)
-		} else {
-			apply(fullPath(f.Name()))
-		}
+
+	return &worker{writer: writer, limit: *limit}
+}
+
+func countFiles(_ string, _ os.FileInfo, err error) error {
+	if err != nil {
+		return err
 	}
+	totalFiles++
+	return nil
 }
--- worker.go
+++ worker.go
@@ -9,19 +9,19 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
 )
 
 const gz = ".gz"
 
 type worker struct {
-	id       int
-	con      net.Conn
-	group    *sync.WaitGroup
+	writer   io.WriteCloser
 	closable []io.Closer
+	limit    float64
+	sync.WaitGroup
 }
 
 func (w *worker) Work(fileChan chan string) {
-	w.group.Add(1)
 	for file := range fileChan {
 		w.WorkOnce(file)
 	}
@@ -29,32 +29,47 @@ func (w *worker) Work(fileChan chan string) {
 }
 
 func (w *worker) WorkOnce(file string) {
+	w.Add(1)
+	defer w.Done()
 	scan, err := w.readFile(file)
 	if err != nil {
-		log.Printf("Worker#%v : %s\n", w.id, err)
+		log.Printf("Worker : %s\n", err)
 		return
 	}
-	for scan.Scan() {
-		line := append(scan.Bytes(), '\n')
-		if _, err := w.con.Write(line); err != nil {
-			droppedLines++
-		} else {
-			totalLines++
-		}
-
+	if w.limit == 0 {
+		w.goNoLimit(scan)
+	} else {
+		w.goWithLimit(scan, int64(time.Second)/int64(w.limit*1000))
+	}
+}
+func (w *worker) goNoLimit(scanner *bufio.Scanner) {
+	for scanner.Scan() {
+		w.sendLine(scanner.Bytes())
+	}
+}
+func (w *worker) goWithLimit(scanner *bufio.Scanner, limit int64) {
+	throttle := time.Tick(time.Duration(limit))
+	for scanner.Scan() {
+		<-throttle
+		w.sendLine(scanner.Bytes())
+	}
+}
+func (w *worker) sendLine(data []byte) {
+	if _, err := w.writer.Write(append(data, '\n')); err == nil {
+		totalLines++
+	} else {
+		droppedLines++
 	}
 }
 
 func (w *worker) Close() {
-	w.con.Close()
+	w.writer.Close()
 	for _, cl := range w.closable {
 		cl.Close()
 	}
-	w.group.Done()
 }
 
 func (w *worker) readFile(name string) (*bufio.Scanner, error) {
-
 	file, err := os.Open(name)
 	if err != nil {
 		return nil, err
@@ -82,4 +97,32 @@ func getUDPConnection(addr string) (net.Conn, error) {
 		return nil, err
 	}
 	return conn, nil
+}
+
+type F struct {
+	f  *os.File
+	gf *gzip.Writer
+	fw *bufio.Writer
+}
+
+func (f *F) Write(p []byte) (n int, err error) {
+	return (f.fw).Write(p)
+}
+
+func (f *F) Close() error {
+	f.fw.Flush()
+	f.gf.Close()
+	f.f.Close()
+	return nil
+}
+
+func getGzFile(path string) (f *F, err error) {
+	fi, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0660)
+	if err != nil {
+		return nil, err
+	}
+	gf := gzip.NewWriter(fi)
+	fw := bufio.NewWriter(gf)
+	f = &F{fi, gf, fw}
+	return f, nil
 }
